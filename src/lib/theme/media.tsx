ffde2ca92fd21ed426b549265e58ac5f82839d81
deep-hwan <deep@deepcomu.com>
2023-08-20 16:16:57 +0900

    site_url

---
+++ src/lib/theme/media.tsx
@@ -0,0 +1,2 @@
+export const IMAGE_BLUER: string =
+  "data:image/gif;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAFklEQVR42mN8//HLfwYiAOOoQvoqBABbWyZJf74GZgAAAABJRU5ErkJggg==";
--- src/lib/utils/emum.tsx
+++ src/lib/utils/emum.tsx
@@ -8,6 +8,3 @@ export const TOKEN: { [key: string]: string } = {
   ACCESS: "name_access",
   REFRESH: "name_refresh",
 } as const;
-
-export const IMAGE_BLUER: string =
-  "data:image/gif;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAFklEQVR42mN8//HLfwYiAOOoQvoqBABbWyZJf74GZgAAAABJRU5ErkJggg==";
